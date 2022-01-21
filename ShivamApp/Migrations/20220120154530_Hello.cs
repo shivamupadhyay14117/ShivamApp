f31ce8e1521d5375a05247b12d330c7d382a0c9d
hp <hp@LAPTOP-VSSLG4G6>
2022-01-21 14:43:47 +0530

    Add project files.

---
+++ ShivamApp/Migrations/20220120154530_Hello.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShivamApp.Migrations
+{
+    public partial class Hello : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ ShivamApp/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShivamApp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; } 
+        public int Price { get; set; }
+        public DateTime createDate { get; set; }
+
+        
+
+    }
+}
